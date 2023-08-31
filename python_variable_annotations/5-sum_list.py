e8f47c13659e2eeebbb3f0d8ff5347a492773a59
goever1 <jorgefernandoochoa1@hotmail.com>
2023-08-31 11:18:11 -0500

    Python - Variable Annotations

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -3,6 +3,6 @@
 from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """it returns the sum of the float argument """
     return sum(input_list)
