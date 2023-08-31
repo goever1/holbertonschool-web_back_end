34f1dd9a0b1216e8b2c6aa9f337d07c5d90df295
goever1 <jorgefernandoochoa1@hotmail.com>
2023-08-31 11:47:14 -0500

    Python - Variable Annotations

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Duck type and iteration"""
+from typing import Iterable, Sequence, List, Union, Tuple
+
+
+def element_length(lst: Iterable[Sequence])\
+    -> List[Tuple[Sequence, int]]:
+    """it return values with the appropriate types"""
+    return [(i, len(i)) for i in lst]
