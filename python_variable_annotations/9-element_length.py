32e0c098980aadea6ae5257e1e0010aa8f19594f
goever1 <jorgefernandoochoa1@hotmail.com>
2023-08-31 11:48:21 -0500

    Python - Variable Annotations

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -3,7 +3,6 @@
 from typing import Iterable, Sequence, List, Union, Tuple
 
 
-def element_length(lst: Iterable[Sequence])\
-    -> List[Tuple[Sequence, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """it return values with the appropriate types"""
     return [(i, len(i)) for i in lst]
