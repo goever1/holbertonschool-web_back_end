84513ba1fb5dec9bb2dbe3a675fdfbe77fd9283d
goever1 <jorgefernandoochoa1@hotmail.com>
2023-08-31 11:15:23 -0500

    Python - Variable Annotations

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,13 +1,8 @@
 #!/usr/bin/env python3
 """list of floats annotations"""
+from typing import List
 
 
 def sum_list(input_list: list[float]) -> float:
     """it returns the sum of the float argument """
-
-    res: float = 0
-
-    for x in input_list:
-        res += x
-    
-    return res
+    return sum(input_list)
