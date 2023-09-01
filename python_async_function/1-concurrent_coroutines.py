a4fb96f62b9d05bbfbeb0165ca1f82922a77fb16
goever1 <jorgefernandoochoa1@hotmail.com>
2023-09-01 10:14:13 -0500

    Python - Async

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Async Generation"""
+import asyncio
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax.py').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """It returns a list of all delayed tasks"""
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    delayed_tasks = [await task for task in asyncio.as_completed(tasks)]
+    return delayed_tasks
