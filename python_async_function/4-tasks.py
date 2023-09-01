4bf6a8cd8a55e2ab94d17a277d0253dc0dcffb22
goever1 <jorgefernandoochoa1@hotmail.com>
2023-09-01 11:06:19 -0500

    Python - Async

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -2,12 +2,11 @@
 """Async Generation"""
 from typing import List
 import asyncio
-import time
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """It returns a list"""
-    list = [get(max_delay) for i in range(n)]
+    list = [task_wait_random(max_delay) for i in range(n)]
     return [await task for task in asyncio.as_completed(list)]
