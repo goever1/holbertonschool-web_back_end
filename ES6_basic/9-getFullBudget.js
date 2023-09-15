58e595ffd83eb5167f8c06f147cc941bbba758bb
goever1 <30449167+goever1@users.noreply.github.com>
2023-09-15 10:29:40 -0500

    Add files via upload

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -3,10 +3,10 @@ import getBudgetObject from './7-getBudgetObject';
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
-    ...budget,
-    getIncomeInDollars: (income) => `$${income}`,
-    getIncomeInEuros: (income) => `${income} euros`,
-  };
+  ...budget,
+  getIncomeInDollars: (income) => `$${income}`,
+  getIncomeInEuros: (income) => `${income} euros`,
+};
 
-  return fullBudget;
+return fullBudget;
 }
