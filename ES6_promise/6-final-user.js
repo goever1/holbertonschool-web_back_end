e2da456c004df5046051a2f43a4e2402938b72da
goever1 <30449167+goever1@users.noreply.github.com>
2023-10-12 09:58:30 -0500

    Add files via upload

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,8 +1,23 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
-  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
-}
+const handleProfileSignup = async (
+  firstName,
+  lastName,
+  fileName,
+) => {
+  const result = [];
+  try {
+    const user = await signUpUser(firstName, lastName);
+    result.push({ status: 'fulfilled', value: user });
+    await uploadPhoto(fileName);
+  } catch (error) {
+    result.push({
+      status: 'rejected',
+      value: error.toString(),
+    });
+  }
+  return result;
+};
 
 export default handleProfileSignup;
